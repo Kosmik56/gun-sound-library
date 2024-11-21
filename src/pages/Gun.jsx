a71fcedf20b8f9aa9747a34317ab4dd286be3230
Maxence-Lesage <125356921+Maxence-Lesage@users.noreply.github.com>
2024-11-21 16:33:22 +0100

    gun page with placeholder, allowed users to access the weapon description (#14)

--- src/pages/Gun.jsx
+++ src/pages/Gun.jsx
@@ -5,6 +5,12 @@ export default function Gun() {
 
   const { id } = useParams();
   const theme = useSelector((state) => state.theme.isLight);
+  const data = {
+    title: id,
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi deserunt, libero aperiam ex architecto cumque modi blanditiis accusantium excepturi veniam. Repellat, perferendis ab. Porro quidem vel repellat doloribus praesentium eligendi incidunt commodi non. Natus facere ullam nostrum vel unde dolor odit quam explicabo exercitationem ducimus magnam eligendi tenetur, id dicta!",
+    imageSrc: "/placeholder.png",
+    audioSrc: "/placeholder.mp3"
+  }
 
   if (!theme) {
     document.body.style.backgroundColor = "black";
@@ -15,9 +21,22 @@ export default function Gun() {
   }
 
   return (
-    <>
-      <h1>Gun Page</h1>
-      <p>{id}</p>
-    </>
+    <main className="min-h-screen p-[40px] flex gap-[100px]">
+      <section>
+        <h2 className="text-2xl font-sans">Nom de l'arme : {data.title}</h2>
+        <h3 className="text-2xl mt-[30px] font-sans">Description de l'arme :</h3>
+        <p>{data.description}</p>
+      </section>
+      <section className="flex flex-col gap-[30px]">
+        <img
+          className="w-[350px] bg-gray-100 aspect-video object-cover object-center"
+          src={data.imageSrc}
+          alt=""
+        />
+        <audio controls>
+          <source src={data.audioSrc} type="audio/mpeg" />
+        </audio>
+      </section>
+    </main>
   )
 }
