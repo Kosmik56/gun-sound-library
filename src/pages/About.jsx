fe438f1c86db7fbdf9553bf86e72a0a95236fcaa
Maxence-Lesage <125356921+Maxence-Lesage@users.noreply.github.com>
2024-11-21 15:25:41 +0100

    About Page with placeholders (#10)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,9 +12,21 @@ export default function About() {
     document.body.style.color = "black";
   }
 
+  const header_style = "text-[32px] font-sans";
+  const paragraph_style = "my-[30px] text-[20px] tracking-wide text-pretty";
+
   return (
-    <>
-      <h1>About</h1>
-    </>
+    <main className="px-[40px] my-[40px]">
+      <h2 className={header_style}>👉 Qui sommes-nous ?</h2>
+      <p className={paragraph_style}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo quod dolores illum odio rerum est sint harum dignissimos dolorem maxime unde, quibusdam porro tenetur eveniet cum aspernatur commodi iusto neque corrupti corporis eius officiis. Reprehenderit soluta iure doloribus natus eaque maxime? Itaque praesentium similique laudantium.</p>
+      <h2 className={header_style}>😎 Notre mission</h2>
+      <p className={paragraph_style}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo quod dolores illum odio rerum est sint harum dignissimos dolorem maxime unde, quibusdam porro tenetur eveniet cum aspernatur commodi iusto neque corrupti corporis eius officiis. Reprehenderit soluta iure doloribus natus eaque maxime? Itaque praesentium similique laudantium.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam distinctio quibusdam ab fugiat deleniti architecto atque voluptatibus voluptas maxime corrupti.
+      </p>
+      <h2 className={header_style}>🌟 Pourquoi nous sommes génials ?</h2>
+      <p className={paragraph_style}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo quod dolores illum odio rerum est sint harum dignissimos dolorem maxime unde, quibusdam porro tenetur eveniet cum aspernatur commodi iusto neque corrupti corporis eius officiis. Reprehenderit soluta iure doloribus natus eaque maxime? Itaque praesentium similique laudantium.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </main>
   )
 }
