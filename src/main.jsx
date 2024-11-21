69dd036ee9be11e5472ff9e6607aa4633a569369
Maxence-Lesage <125356921+Maxence-Lesage@users.noreply.github.com>
2024-11-21 13:34:54 +0100

    Added Navbar with navigations links and logo inside Header component (#7)
    LGTM

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
-import {store} from './redux/store.js'
+import { store } from './redux/store.js'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import Gun from './pages/Gun.jsx'
 import About from './pages/About.jsx'
+import Header from './components/header/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Navbar />
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/gun/:id" element={<Gun />} />
